f62f825c4d792211f303349f0b3131bb246a2b11
Steffen Birkeland <Steffenf.birkeland@gmail.com>
2014-08-28 15:08:12 +0200

    ikke noe spess

--- awsomesite/urls.py
+++ awsomesite/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls import patterns, include, url
 
 
 # Steffen var her
+# Jeg var her igjen
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'awsomesite.views.home', name='home'),
