2c5c0c5f0fe27c6464e5f08c0709b39832b333eb
Lars Brusletto <larsbrusletto@gmail.com>
2014-09-02 14:01:43 +0200

    added comment avout database on heroku

--- awsomesite/settings.py
+++ awsomesite/settings.py
@@ -21,6 +21,10 @@ DATABASES = {
     }
 }
 
+#import dj_database_url 
+#DATABASES['default'] = dj_database_url.config()  # postgres://USER:PASSWORD@HOST:PORT/NAME
+#DATABASES['default'] =  dj_database_url.config()
+
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
